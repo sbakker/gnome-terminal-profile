03e745c8cd0e2b4f9cccc3a38dbdad9446f337a1
Steven Bakker <steven.bakker@ams-ix.net>
2017-11-27 16:52:34 +0100

    Add "compilation", expand and fix POD.

--- src/parse_yaml_theme.pl
+++ src/parse_yaml_theme.pl
@@ -7,11 +7,10 @@
 #
 #  Description:  Create gnome-terminal theme from YAML file.
 #
-#        Notes:  ---
 #       Author:  Steven Bakker (SB), <Steven.Bakker@ams-ix.net>
 #      Created:  17/12/14
 #
-#   Copyright (c) 2014 Steven Bakker; All rights reserved.
+#   Copyright (c) 2014-2017 Steven Bakker; All rights reserved.
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the same terms as Perl itself. See "perldoc perlartistic".
@@ -27,11 +26,12 @@ use FindBin;
 use Getopt::Long;
 use Pod::Usage;
 use YAML;
+use File::Basename;
 use Getopt::Long;
 
-$::VERSION = '1.00';
+$::VERSION = '1.01';
 my $app_header = "\nThis is $FindBin::Script, v$::VERSION\n\n"
-			   . "See \"perldoc $FindBin::Script\" for more information.\n"
+               . "See \"perldoc $FindBin::Script\" for more information.\n"
                ;
 
 # Default "Tango" Scheme.
@@ -49,40 +49,130 @@ my @dfl_palette = (
 Getopt::Long::Configure("bundling");
 
 GetOptions(
-	'help|?'     => sub {
+    'help|?'     => sub {
         pod2usage(-msg => $app_header, -exitval=>0, -verbose=>0)
     },
-	'manual'     => sub { pod2usage(-exitval=>0, -verbose=>2) },
-	'version|V'  => sub { print $app_header; exit(0) },
+    'manual'     => sub { pod2usage(-exitval=>0, -verbose=>2) },
+    'version|V'  => sub { print $app_header; exit(0) },
     # Custom options.
 ) or pod2usage(-exitval=>2);
 
 pod2usage(-message => "\nNeed at least one argument\n", -exitval => 2)
-	unless @ARGV >= 1;
+    unless @ARGV >= 1;
 
 my $file = shift @ARGV;
 
-my $str = do {
-  open my $fh, "<", $file or die "$FindBin::Script: cannot read $file: $!\n";
-  local($/) = undef;
-  <$fh>;
-};
+my ($theme_ref, $palette_ref) = parse_theme($file);
 
-my $hashref = Load( $str );
-my @palette;
-for my $i (0..7) {
-    my $color = ($hashref->{"color$i"} //= $dfl_palette[$i]);
-    if (ref $color eq 'ARRAY') {
-        @$color = rgb2hex(@$color);
-        $palette[$i]   = $color->[0];
-        $palette[$i+8] = $color->[1];
+my %scheme = (
+    'bg' => $theme_ref->{bg},
+    'fg' => $theme_ref->{fg},
+    'bd' => $theme_ref->{bd},
+    'palette_dconf' => join(', ', map { qq{'$_'} } @$palette_ref),
+    'palette_gconf' => join(":", @$palette_ref),
+);
+
+if (@ARGV) {
+    for my $key (@ARGV) {
+        if (exists($scheme{$key})) {
+            say $scheme{$key};
+        }
+        elsif ($key eq 'show') {
+            show_palette(\%scheme, $palette_ref);
+        }
+        elsif ($key eq 'yaml') {
+            print YAML::Dump($theme_ref);
+        }
+        elsif ($key eq 'shell') {
+            say "bd_color='$scheme{bd}'";
+            say "bg_color='$scheme{bg}'";
+            say "fg_color='$scheme{fg}'";
+            say q{palette_dconf="},
+                $scheme{'palette_dconf'}, q{"};
+            say q{palette_gconf='},
+                $scheme{'palette_gconf'}, q{'};
+        }
+        elsif ($key eq 'compile') {
+            compile_scheme($file, $theme_ref, $palette_ref);
+        }
+        else {
+            die "$FindBin::Script: bad key \"$key\"\n";
+        }
     }
-    else {
-        $color = rgb2hex($color);
-        $palette[$i] = $palette[$i+8] = $color;
-    }
-    $hashref->{"color$i"} = $color;
 }
+else {
+    say "bd_color='$scheme{bd}'";
+    say "bg_color='$scheme{bg}'";
+    say "fg_color='$scheme{fg}'";
+    say "palette=( ", join(" ", map { "'$_'" } @$palette_ref), " )";
+}
+
+
+sub parse_theme {
+    my $file = shift;
+
+    my $theme_ref = eval { YAML::LoadFile( $file ) };
+    if (my $err = $@) {
+        $err =~ s/\n* at .*? line \d+\.\n*$//;
+        die "*** $FindBin::Script: $err\n";
+    }
+
+    my @palette;
+    for my $i (0..7) {
+        my $color = ($theme_ref->{"color$i"} //= $dfl_palette[$i]);
+        if (ref $color eq 'ARRAY') {
+            @$color = rgb2hex(@$color);
+            $palette[$i]   = $color->[0];
+            $palette[$i+8] = $color->[1];
+        }
+        else {
+            $color = rgb2hex($color);
+            $palette[$i] = $palette[$i+8] = $color;
+        }
+        $theme_ref->{"color$i"} = $color;
+    }
+
+    $theme_ref->{bg} //= $palette[0];
+    $theme_ref->{fg} //= $palette[1];
+    $theme_ref->{bd} //= $theme_ref->{fg};
+    return ($theme_ref, \@palette);
+}
+
+
+sub compile_scheme {
+    my ($file, $theme_ref, $palette_ref) = @_;
+
+    my $output_dir = dirname($file);
+    for my $key (qw( bd bg fg )) {
+        write_file("$output_dir/${key}_color", $theme_ref->{$key}, "\n");
+    }
+
+    write_file("$output_dir/palette", map { "$_\n" } @$palette_ref);
+}
+
+
+sub write_file {
+    my $out_fname = shift;
+
+    print "writing $out_fname: ";
+
+    my $out_fh;
+    if (!open $out_fh, ">", $out_fname) {
+        say "ERROR";
+        die "*** $FindBin::Script: cannot write to $out_fname: $!\n";
+    }
+
+    print $out_fh @_;
+
+    if (! close $out_fh ) {
+        say "ERROR";
+        die "*** $FindBin::Script: error writing to $out_fname: $!\n";
+    }
+
+    say "ok";
+    return 1;
+}
+
 
 sub rgb2hex {
     my @result;
@@ -98,49 +188,6 @@ sub rgb2hex {
     return @result == 1 ? $result[0] : @result;
 }
 
-$hashref->{bg} //= $palette[0];
-$hashref->{fg} //= $palette[1];
-$hashref->{bd} //= $hashref->{fg};
-
-my %scheme = (
-    'bg' => $hashref->{bg},
-    'fg' => $hashref->{fg},
-    'bd' => $hashref->{bd},
-    'palette_dconf' => join(', ', map { qq{'$_'} } @palette),
-    'palette_gconf' => join(":", @palette),
-);
-
-if (@ARGV) {
-    for my $key (@ARGV) {
-        if (exists($scheme{$key})) {
-            say $scheme{$key};
-        }
-        elsif ($key eq 'show') {
-            show_palette(\%scheme, \@palette);
-        }
-        elsif ($key eq 'yaml') {
-            print Dump($hashref);
-        }
-        elsif ($key eq 'shell') {
-            say "bd_color='$scheme{bd}'";
-            say "bg_color='$scheme{bg}'";
-            say "fg_color='$scheme{fg}'";
-            say q{palette_dconf="},
-                $scheme{'palette_dconf'}, q{"};
-            say q{palette_gconf='},
-                $scheme{'palette_gconf'}, q{'};
-        }
-        else {
-            die "$FindBin::Script: bad key \"$key\"\n";
-        }
-    }
-}
-else {
-    say "bd_color='$scheme{bd}'";
-    say "bg_color='$scheme{bg}'";
-    say "fg_color='$scheme{fg}'";
-    say "palette=( ", join(" ", map { "'$_'" } @palette), " )";
-}
 
 sub parse_rgb {
     my $rgb = shift;
@@ -155,6 +202,7 @@ sub parse_rgb {
     }
 }
 
+
 sub rgb_str {
     my ($str, $bg, $fg) = @_;
 
@@ -166,8 +214,9 @@ sub rgb_str {
          . "$str\x1b[0m";
 }
 
+
 sub show_palette {
-    my ($scheme, $palette) = @_;
+    my ($scheme, $palette_ref) = @_;
 
     print "bd_color: |", rgb_str("  ", $scheme->{bd}), "|\n";
     print "fg_color: |", rgb_str("  ", $scheme->{fg}), "|\n";
@@ -189,7 +238,7 @@ sub show_palette {
     for my $i (0..7) {
         print " ", rgb_str(
             '       ',
-            $palette[$i],
+            $palette_ref->[$i],
         );
     }
     print "\n";
@@ -197,7 +246,7 @@ sub show_palette {
     for my $i (8..15) {
         print " ", rgb_str(
             '       ',
-            $palette[$i],
+            $palette_ref->[$i],
         );
     }
     print "\n";
@@ -213,7 +262,7 @@ parse_yaml_theme.pl - create gnome-terminal theme values from a YAML file
 
 B<parse_yaml_theme.pl>
 I<YAML-file>
-[B<show>|B<bd>|B<bg>|B<fg>|B<palette_dconf>|B<palette_gconf>|B<yaml>|B<shell>] ...
+[B<show>|B<bd>|B<bg>|B<fg>|B<palette_dconf>|B<palette_gconf>|B<yaml>|B<shell>|B<compile>] ...
 
 =head1 DESCRIPTION
 
@@ -273,12 +322,33 @@ above input have been expanded and replaced):
       - '#DCDCDCDCCCCC'
     fg: '#BABABDBDB6B6'
 
+=head2 Compiled Output
+
+To produce output files that are compatible with the default C<install.sh> of
+L<Anthony's gnome-terminal-colors-solarized>, run this command:
+
+    $ parse_yaml_theme.pl steven.yaml compile
+    writing bd_color: ok
+    writing bg_color: ok
+    writing fg_color: ok
+    writing palette: ok
+
+It will create F<bd_color>, F<bg_color>, F<fg_color>, and F<palette> files in
+same directory as the YAML file.
+
 =head2 Palette Output
 
 Print the palette colors on the terminal (only works for gnome-terminal
 3.12 and above):
 
     $ parse_yaml_theme.pl steven.yaml show
+    bd_color: |**|
+    fg_color: |++|
+    bg_color: |--|
+    palette:
+             Black    Red    Green  Yellow   Blue   Magenta  Cyan    White
+    Normal: [-----] [-----] [-----] [-----] [-----] [-----] [-----] [-----]
+    Bright: [+++++] [+++++] [+++++] [+++++] [+++++] [+++++] [+++++] [+++++]
 
 =head2 Default Output
 
@@ -389,7 +459,7 @@ Steven Bakker E<lt>Steven.Bakker@ams-ix.netE<gt>; 2014.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2014 Steven Bakker; All rights reserved.
+Copyright (c) 2014-2017 Steven Bakker; All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. See "perldoc perlartistic".
